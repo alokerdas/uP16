b17aaf908b1d875e8822d7cf493c849a31fc07d3
Aloke Das <aloke.das@ieee.org>
2022-08-01 11:34:34 +0530

    Simplified memory model

--- rtl/memory.v
+++ rtl/memory.v
@@ -1,32 +1,22 @@
 /*____________________________________________________________________________*/                     
 /////////////////////////*  MEMORY ( 16 X 12 bit )  *///////////////////////////________________________________________________________________________________********************************************************************************
                                                                                
-module memory (clock, addr, data, rdwr, en, ack);
+module memory (clock, addr, datain, dataout, rdwr, en);
   input [11:0] addr;
-  inout [15:0] data;
+  input [15:0] datain;
   input clock, rdwr, en;
-  output ack;
+  output [15:0] dataout;
 
   reg [15:0] dataout;
-  reg ack;
   reg [15:0] mem [0:4095];
  
-  initial begin
-    $readmemh("prog.mem", mem);
-  end
-
-  assign data = dataout;
   always @ (posedge clock) begin
     if (en) begin
       if (rdwr) begin
-        mem[addr] <= data;
+        mem[addr] <= datain;
       end else begin
         dataout <= mem[addr];
       end
-      ack <= 1'b1;
-    end else begin
-      dataout <= 16'hzzzz;
-      ack <= 1'b0;
     end
   end
 
